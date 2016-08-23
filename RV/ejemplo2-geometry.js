a05e6240528981acadc8ac0e80a3b5fa9188ee69
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-08-23 15:43:03 -0500

    Update ejemplo2-geometry.js

--- RV/ejemplo2-geometry.js
+++ RV/ejemplo2-geometry.js
@@ -1,9 +1,9 @@
 var forma=new THREE.Geometry();
-forma.vertices.push(new THREE.Vector3(1,  0,  1));
-forma.vertices.push(new THREE.Vector3(1,  0,  -1));
-forma.vertices.push(new THREE.Vector3(-1,  0,  -1));
-forma.vertices.push(new THREE.Vector3(-1,  0,  1));
-forma.vertices.push(new THREE.Vector3(0,  1,  0));
+forma.vertices.push(new THREE.Vector3(1,0,1));
+forma.vertices.push(new THREE.Vector3(1,0,-1));
+forma.vertices.push(new THREE.Vector3(-1,0,-1));
+forma.vertices.push(new THREE.Vector3(-1,0,1));
+forma.vertices.push(new THREE.Vector3(0,1,0));
 
 forma.faces.push(new THREE.Face3(3,2,1));
 forma.faces.push(new THREE.Face3(3,1,0));
@@ -12,7 +12,7 @@ forma.faces.push(new THREE.Face3(0,1,4));
 forma.faces.push(new THREE.Face3(1,2,4));
 forma.faces.push(new THREE.Face3(2,3,4));
 
-forma.computeBoundingSphere();
+forma.computerBoundingSphere();
 forma.computerFaceNornals();
 
 var material=new THREE.MeshNormalMaterial();
