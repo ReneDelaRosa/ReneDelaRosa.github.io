422e9fb8f000adf08e4983e92a79e73d2996577c
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-08-29 16:28:34 -0500

    Update ejercicio2.js

--- RV/ejercicio2.js
+++ RV/ejercicio2.js
@@ -8,13 +8,14 @@ document.body.appendChild(renderizador.domElement);
 camara.position.z=500;
 
 
-var forma1=new THREE.CylinderGeometry(20,20,300,30);
+var forma1=new THREE.CylinderGeometry(20,20,200,30);
 var forma2=new THREE.SphereGeometry(50,32,32);
 var material1=new THREE.MeshBasicMaterial( { color: 0x84550A } );
 var material2=new THREE.MeshBasicMaterial( { color: 0x04AA04});
 var tronco=new THREE.Mesh(forma1, material1);
 var copa=new THREE.Mesh(forma2,material2);
 copa.position.y=60;
+tronco.position.y=-15;
 escena.add(tronco);
 escena.add(copa);
 renderizador.render(escena,camara);
