9746a3a2e164fde4725e671fb53260dcfea8f9c9
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-08-29 16:00:53 -0500

    Update ejercicio2.js

--- RV/ejercicio2.js
+++ RV/ejercicio2.js
@@ -1,9 +1,13 @@
 var escena=new THREE.Scene();
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=5;
+
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
+
+camara.position.z=5;
+
+
 var forma=new THREE.BoxGeometry(1,1,1);
 var material=new THREE.MeshNormalMaterial();
 var cubo=new THREE.Mesh(forma, material);
