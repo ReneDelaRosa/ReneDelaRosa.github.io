e6bb7eb1966301fe2036fed1774ec7a081fd706f
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-08-30 15:27:44 -0500

    Creación de cubo con vertices

--- RV/ejercicio3.js
+++ RV/ejercicio3.js
@@ -26,13 +26,13 @@ forma.computeFaceNormals();
 
 var material= new THREE.MeshNormalMaterial();
 var malla=new THREE.Mesh(forma,material);
-//malla.rotateX(Math.PI/6);
-malla.rotateY(Math.PI/2);
+malla.rotateX(Math.PI/4);
+malla.rotateY(Math.PI/4);
 
 var escena= new THREE.Scene();
 escena.add(malla);
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=4;
+camara.position.z=5;
 
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
