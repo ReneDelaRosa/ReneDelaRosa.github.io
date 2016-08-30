02fcfbfa4d1181eebac38c61be615b4bc9273da0
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-08-30 11:00:46 -0500

    Creación de cubo con union de vertices

--- RV/ejercicio3.js
+++ RV/ejercicio3.js
@@ -19,7 +19,7 @@ forma.faces.push(new THREE.Face3(5,1,6));
 forma.faces.push(new THREE.Face3(4,3,5));
 forma.faces.push(new THREE.Face3(5,3,0));
 forma.faces.push(new THREE.Face3(2,7,6));
-forma.faces.push(new THREE.Face3(6,1,2));
+forma.faces.push(new THREE.Face3(6,2,1));//Marcada
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
