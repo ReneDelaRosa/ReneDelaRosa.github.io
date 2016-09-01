43c6d05254c3bdf7fa040c5d16ebc934a3017f08
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-01 15:43:32 -0500

    Creación de una estrella 2d

--- RV/ejercicio4.js
+++ RV/ejercicio4.js
@@ -1,1 +1,27 @@
+var figura=new THREE.Shape();
+figura.moveTo(0,1.6);
+figura.lineTo(-0.76,3.8);
+figura.lineTo(-3.24,3.8);
+figura.lineTo(-1.24,2.35);
+figura.lineTo(-2,0);
+figura.lineTo(0,1.45);
+figura.lineTo(2,0);
+figura.lineTo(1.24,2.35);
+figura.lineTo(3.24,3.8);
+figura.lineTo(0.76,3.8);
 
+var forma=new THREE.ShapeGeometry(figura);
+var malla=new THREE.Mesh(forma)
+
+var escena=new THREE.Scene();
+escena.add(malla);
+
+var camara=new THREE.PerspectiveCamera();
+camara.position.x=0;
+camara.position.y=2.75;
+camara.position.z=10;
+
+var renderizador=new THREE.WebGLRenderer();
+renderizador.setsize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
