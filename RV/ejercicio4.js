7f6779fa8929c2d76fdf7639ca7ed65853ffd4b6
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-01 15:48:40 -0500

    Creación de una estrella 2d

--- RV/ejercicio4.js
+++ RV/ejercicio4.js
@@ -23,6 +23,6 @@ camara.position.y=2.75;
 camara.position.z=10;
 
 var renderizador=new THREE.WebGLRenderer();
-renderizador.setsize(window.innerHeight*.95,window.innerHeight*.95);
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
