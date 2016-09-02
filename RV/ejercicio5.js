9a2214158db90337f76d145ba84b1e4bdb9ce0ab
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-02 17:17:39 -0500

    Update ejercicio5.js

--- RV/ejercicio5.js
+++ RV/ejercicio5.js
@@ -34,9 +34,9 @@ escena.add(malla);
 escena.add(malla1);
 escena.add(malla2);
 
-malla.rotateX(Math.PI/2);
-malla1.rotateX(Math.PI/2);
-malla2.rotateX(Math.PI/2);
+malla.rotateX(Math.PI/3);
+malla1.rotateX(Math.PI/3);
+malla2.rotateX(Math.PI/3);
 //malla2.rotateY(Math.PI);
 
 var camara=new THREE.PerspectiveCamera();
