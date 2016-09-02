f44d82108fed26682be2a0cc190c85b097189083
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-02 08:07:23 -0500

    Update ejercicio5.js

--- RV/ejercicio5.js
+++ RV/ejercicio5.js
@@ -1,14 +1,14 @@
 var puntos[
-  new THREE.Vector2(0.1,0);
-  new THREE.Vector2(2,0.0);
-  new THREE.Vector2(2,0.5);
-  new THREE.Vector2(1.75,0.5);
-  new THREE.Vector2(1.75,1);
-  new THREE.Vector2(1.2,1.8);
-  new THREE.Vector2(1.2,5.5);
-  new THREE.Vector2(1.75,6.3);
-  new THREE.Vector2(1.75,6.6);
-  new THREE.Vector2(0.1,6.6);
+  new THREE.Vector2(0.1,0),
+  new THREE.Vector2(2,0.0),
+  new THREE.Vector2(2,0.5),
+  new THREE.Vector2(1.75,0.5),
+  new THREE.Vector2(1.75,1),
+  new THREE.Vector2(1.2,1.8),
+  new THREE.Vector2(1.2,5.5),
+  new THREE.Vector2(1.75,6.3),
+  new THREE.Vector2(1.75,6.6),
+  new THREE.Vector2(0.1,6.6)
 ];
 var revotorre= new THREE.LatheGeometry(puntos);
 var material=new THREE.MeshNormalMaterial();
