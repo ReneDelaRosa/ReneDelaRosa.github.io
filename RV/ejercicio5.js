0927e5c087f4d39ba6382c021feffe6d3462f06d
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-02 23:43:30 -0500

    Update ejercicio5.js

--- RV/ejercicio5.js
+++ RV/ejercicio5.js
@@ -32,19 +32,19 @@ figura.lineTo(0,1.75);
 var estrella=new THREE.ExtrudeGeometry(figura,{amount:10});
 //var estrella=new THREE.ShapeGeometry(figura);
 //Revolucion
-var revotorre= new THREE.LatheGeometry(puntos,32);
+//var revotorre= new THREE.LatheGeometry(puntos,32);
 //Creacion de las mallas
-var malla=new THREE.Mesh(revotorre);
-var malla1=new THREE.Mesh(cilindro);
+//var malla=new THREE.Mesh(revotorre);
+//var malla1=new THREE.Mesh(cilindro);
 
 //Creacion de la figura final
-var torre=new THREE.Geometry();
+//var torre=new THREE.Geometry();
 //Union de las mallas
-torre.merge(malla.geometry, malla.matrix);
-torre.merge(malla1.geometry, malla1.matrix);
+//torre.merge(malla.geometry, malla.matrix);
+//torre.merge(malla1.geometry, malla1.matrix);
 //Asignación del material a la malla final
 var material=new THREE.MeshNormalMaterial();
-var torreMalla=new THREE.Mesh(torre,material);
+//var torreMalla=new THREE.Mesh(torre,material);
 
 var malla2=new THREE.Mesh(estrella,material);
 
@@ -52,7 +52,7 @@ var escena=new THREE.Scene();
 //escena.add(torreMalla);
 escena.add(malla2);
 
-torreMalla.rotateX(Math.PI/4);
+//torreMalla.rotateX(Math.PI/4);
 
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=15;
