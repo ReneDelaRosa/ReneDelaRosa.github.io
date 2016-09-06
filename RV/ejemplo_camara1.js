f4694b0f4b473953926c72e3a6cf0368dd4e883b
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-06 15:32:19 -0500

    Ejemplo con cámara ortográfica

--- RV/ejemplo_camara1.js
+++ RV/ejemplo_camara1.js
@@ -1,7 +1,7 @@
 var camara=new THREE.OrthographicCamera();
 
-camara.left=window.innerHeight/-2;
-camara.right=window.innerHeight/2;
+camara.left=window.innerWidth/-2;
+camara.right=window.innerWidth/2;
 camara.top=window.innerHeight/2;
 camara.bottom=window.innerHeight/-2;
 camara.near=0.1;
@@ -12,11 +12,11 @@ camara.position.z=100;
 var cubo=new THREE.Mesh(new THREE.BoxGeometry(30,30,30),new THREE.MeshNormalMaterial());
 cubo.rotateY(Math.PI/4);
 
-var esfera1=new THREE.Mesh(new THREE.SphereGeometry(15),new THREE.MeshNormalMaterial());
-esfera1.position.x=50;
+var esfera1=new THREE.Mesh(new THREE.SphereGeometry(50),new THREE.MeshNormalMaterial());
+esfera1.position.x=100;
 
-var esfera2=new THREE.Mesh(new THREE.SphereGeometry(15),new THREE.MeshNormalMaterial());
-esfera2.position.x=-50;
+var esfera2=new THREE.Mesh(new THREE.SphereGeometry(25),new THREE.MeshNormalMaterial());
+esfera2.position.x=-100;
 esfera2.position.z=-100;
 
 var escena=new THREE.Scene();
