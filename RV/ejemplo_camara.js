953ed7542dd2212154c6fcd63c464662f125c9d9
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-06 14:59:03 -0500

    Ejemplo con camara

--- RV/ejemplo_camara.js
+++ RV/ejemplo_camara.js
@@ -1,5 +1,5 @@
 var campoVision=45;
-var relacionAspecto=windows.innerWidth/ window.innerHeight;
+var relacionAspecto=window.innerWidth/ window.innerHeight;
 var planoCercano=1;
 var planoLejano=1000;
 var camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
