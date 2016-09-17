5d64ac92ed694e7ad20adffc64fc9c9782eb13b2
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-17 02:20:40 -0500

    Update tablero1.js

--- RV/tablero1.js
+++ RV/tablero1.js
@@ -89,8 +89,7 @@ camara.updateProjectionMatrix();
 camara.position.z=200;
 camara.position.x=40;
 camara.position.y=40;
-//camara.lookAt(g1.position);
-//escena.rotateX(-Math.PI/4)
+escena.rotateX(-Math.PI/6)
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
