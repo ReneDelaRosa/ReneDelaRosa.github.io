c719018ae7dcee238317e26ba1c6cbc0de4f9b70
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-17 00:42:45 -0500

    Update torrew.js

--- RV/torrew.js
+++ RV/torrew.js
@@ -50,4 +50,5 @@ var torreMalla=new THREE.Mesh(torre);
 //Union de las mallas2
 torre2.merge(torreMalla.geometry, torreMalla.matrix);
 torre2.merge(malla2.geometry, malla2.matrix);
+torre2.rotateX(Math.PI/2);
 var torrew=new THREE.Mesh(torre2, material);
