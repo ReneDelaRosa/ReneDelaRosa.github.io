0424a49305b31c81bce7a52bd50d412a336b91e4
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-20 17:26:33 -0500

    Update tablero3.js

--- RV/tablero3.js
+++ RV/tablero3.js
@@ -90,8 +90,8 @@ escena.add(torre1);
 escena.add(torre2);
 escena.add(torre3);
 escena.add(torre4);
-escena.add(luzPuntual);
-//escena.add(luzPuntual1);
+//escena.add(luzPuntual);
+escena.add(luzPuntual1);
 //escena.add(luzPuntual2);
 
 var camara=new THREE.PerspectiveCamera();
