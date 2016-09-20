09a6d2eba7655ca36c32c089d74321c58186ea7d
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-20 17:47:38 -0500

    Update tablero3.js

--- RV/tablero3.js
+++ RV/tablero3.js
@@ -99,7 +99,7 @@ var camara=new THREE.PerspectiveCamera();
 camara.position.z=130;
 camara.position.x=45;
 camara.position.y=45;
-//escena.rotateX(-Math.PI/4)
+escena.rotateX(-Math.PI/6)
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
