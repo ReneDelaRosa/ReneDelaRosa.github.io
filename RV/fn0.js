b77385d198678749fe2976aff5766f3ef9aff71a
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-22 14:58:26 -0500

    Creación de funciones

---
+++ RV/fn0.js
@@ -0,0 +1,13 @@
+function init(p){
+  var malla(new THREE.BoxGeometry(p,p,p),new THREE.MeshNormalMaterial());
+  escena=new THREE.Scene();
+  escena.add(malla)
+  camara=new THREE.PerspectiveCamera();
+  renderizador=new THREE.WebGLRenderer();
+  renderizador.setSize(100,700);
+  document.body.appendChild(renderizador.domElement);
+}
+
+var main=function(p){
+renderizador.render(escena,camara);
+}
