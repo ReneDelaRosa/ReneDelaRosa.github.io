b3bc17b25131591b84ff35facf3a9a8e5f9ea6cc
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-22 15:22:49 -0500

    Update fn0.js

--- RV/fn0.js
+++ RV/fn0.js
@@ -4,16 +4,18 @@ function init(p){
   escena.add(malla);
   camara=new THREE.PerspectiveCamera();
   camara.position.z=p*5;//Poscamara
-  malla.rotateX=(Math.PI/6)
+  malla.rotateX(Math.PI/6);
   renderizador=new THREE.WebGLRenderer();
   renderizador.setSize(900,700);
   document.body.appendChild(renderizador.domElement);
 }
 
 var main=function(p){
+  P(1)
 renderizador.render(escena,camara);
 }
 
 var escena,camara,renderizador;
-init(1);
-main(2);
+//init(1);
+//main(2);
+main(init);
