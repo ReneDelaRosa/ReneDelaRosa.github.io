668622f8828871c692b6933c422e5d8bf88ad307
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-27 15:02:29 -0500

    Update ejemplo_ventana.js

--- RV/ejemplo_ventana.js
+++ RV/ejemplo_ventana.js
@@ -19,7 +19,7 @@ VENTANA.setup=function(){
 }
 
 VENTANA.loop=function(){
-  requestAnimationFrame(VENTANA loop);
+  requestAnimationFrame(VENTANA.loop);
   VENTANA.malla.rotateX(0.01);
   VENTANA.malla.rotateY(0.01);
   VENTANA.renderizador.render(VENTANA.scena,VENTANA.camara);
