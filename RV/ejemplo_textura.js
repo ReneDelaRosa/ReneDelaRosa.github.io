e6a9c1ab502e0e8dee4550b438525155dea19833
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-27 15:49:37 -0500

    Update ejemplo_textura.js

--- RV/ejemplo_textura.js
+++ RV/ejemplo_textura.js
@@ -12,7 +12,7 @@ TEXTURA.setup=function(){
   TEXTURA.camara=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
   TEXTURA.camara.position.z=5;
   
-  var lienzo= document.getElementByID("ejemplo_textura");
+  var lienzo= document.getElementById("ejemplo_textura");
   TEXTURA.renderizador=new THREE.WebGLRenderer({canvas:lienzo,antialias:true});
   TEXTURA.renderizador.setSize(600,600);
 }
