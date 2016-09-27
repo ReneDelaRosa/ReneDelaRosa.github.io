ce4ab26dbdf5baad5157616ef3416099bc933a19
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-27 15:05:28 -0500

    Update ejemplo_ventana.js

--- RV/ejemplo_ventana.js
+++ RV/ejemplo_ventana.js
@@ -11,7 +11,7 @@ VENTANA.setup=function(){
   window.addEventListener(tipo_evento,VENTANA.listener, capturarp);
   VENTANA.escena=new THREE.Scene();
   VENTANA.camara=new THREE.PerspectiveCamera(75,window,innerWidth/window.innerHeight,0.1,1000);
-  VENTANA.Camara.position.z=5;
+  VENTANA.camara.position.z=5;
   var lienzo=document.getElementById("ejemplo-ventana");
   VENTANA.renderizador=new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
   VENTANA.malla=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
