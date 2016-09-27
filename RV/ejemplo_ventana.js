63cbe3b063359080f3f3ebdf0d74acdb4590ab5f
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-27 15:05:51 -0500

    Update ejemplo_ventana.js

--- RV/ejemplo_ventana.js
+++ RV/ejemplo_ventana.js
@@ -1,7 +1,7 @@
 var VENTANA=new Object();
 VENTANA.listener=function(){
   VENTANA.camara.aspect=window.innerWidth/window.innerHeight;
-  VENTANA.camara,updateProjectMatrix();
+  VENTANA.camara.updateProjectMatrix();
   VENTANA.renderizador.setSize(window.innerWidth,window.innerHeight);
 }
 VENTANA.setup=function(){
