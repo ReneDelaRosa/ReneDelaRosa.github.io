c7b75088a254e073d0ae412670929a357e9e6ace
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-09-29 16:08:12 -0500

    Update tablero4.js

--- RV/tablero4.js
+++ RV/tablero4.js
@@ -1,1 +1,22 @@
 https://luishdzupiita.github.io/rv/tablerotextura.js
+
+
+cargador1.load(imagen1,retro1)
+cargador4.load(imagen4,retro4)
+
+function setup(){
+  if ((textura!==undefined)&&(textura2!==undefined)&&(textura3!==undefined)&&(textura4!==undefined)&&!setupDone){
+  setupDone=true;
+  }
+  Geometry
+  Material
+  Formas
+  Escena
+  Renderizador
+  Camara
+}
+
+function loop(){
+  Request...Frame(loop)
+  setuo();
+}
