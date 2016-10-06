22ff31a2f7b9d7fb757a1e9eb15fd7ab794f1c87
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-10-06 14:54:38 -0500

    Create aiudaaaaa.js

---
+++ RV/aiudaaaaa.js
@@ -0,0 +1,20 @@
+//Primera parte llama a los cargadores y se sale
+//Segunda parte es ejecutar el loop
+
+var setupDone=false;
+function loop(){
+requestAnimationFrame(loop)
+if(textura1!==undefined && textura2!==undefined && !setupDone)
+setup2();
+.
+.
+.
+}
+
+function setup2(){
+setupDone=true;
+.
+.
+.
+
+}
