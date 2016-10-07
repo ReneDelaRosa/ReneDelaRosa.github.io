8a48adab105080f5df4552783bfff5843206120e
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-10-07 13:04:36 -0500

    Update tablero4.js

--- RV/tablero4.js
+++ RV/tablero4.js
@@ -58,59 +58,147 @@ var bloque=new THREE.BoxGeometry(10,10,10);
 //Declaración del objeto
 var TEXTURA=new object();
 //Declaración de las retrollamadas
-  //Marmol blanco
+//Marmol blanco
 TEXTURA.retrollamadamblanco=function(textura){
   var material=new THREE.MeshBasicMaterial({map:textura});
-  TEXTURA.malla=new THREE.Mesh(torre2,material);
-  TEXTURA.escena.add(TEXTURA.malla);  
+  TEXTURA.ftorre1=new THREE.Mesh(torre2,material);
+  TEXTURA.ftorre2=new THREE.Mesh(torre2,material);
+  ftorre1.position.x=10;
+  ftorre1.position.y=10;
+  ftorre1.position.z=5;
+  ftorre2.position.x=80;
+  ftorre2.position.y=10;
+  ftorre2.position.z=5;
+  ftorre1.scale.set(1.5,1.5,1.5)
+  ftorre2.scale.set(1.5,1.5,1.5)
+TEXTURA.escena.add(TEXTURA.ftorre1);
+TEXTURA.escena.add(TEXTURA.ftorre2); 
 }
-  //Marmol negro
+//Marmol negro
 TEXTURA.retrollamadamnegro=function(textura){
    var material=new THREE.MeshBasicMaterial({map:textura});
-  TEXTURA.malla1=new THREE.Mesh(torre2,material);
-  TEXTURA.escena.add(TEXTURA.malla1);
+   TEXTURA.ftorre3=new THREE.Mesh(torre3,material);
+   TEXTURA.ftorre4=new THREE.Mesh(torre4,material);
+   ftorre3.position.x=10;
+   ftorre3.position.y=80;
+   ftorre3.position.z=5;
+   ftorre4.position.x=80;
+   ftorre4.position.y=80;
+   ftorre4.position.z=5;
+   ftorre3.scale.set(1.5,1.5,1.5)
+   ftorre4.scale.set(1.5,1.5,1.5)
+TEXTURA.escena.add(TEXTURA.ftorre3);
+TEXTURA.escena.add(TEXTURA.ftorre4); 
 }
-  //Ceramica blanca
+//Ceramica blanca
 TEXTURA.retrollamadacblanca=function(textura){
   var material=new THREE.MeshBasicMaterial({map:textura});
-
-  TEXTURA.malla2=new THREE.Mesh();
-  TEXTURA.escena.add(TEXTURA.malla2);
+//Creación del grupo del tablero
+   var g1=new THREE.Group();
+   var k=0;
+    for (var i=0;i<8;i++){
+      for(var j=0;j<8;j++){
+        if(k%2!=0){
+        TEXTURA.malla2=new THREE.Mesh(bloque,material);
+        }
+        TEXTURA.malla2.position.x=(j+1)*10;//Columnas
+        TEXTURA.malla2.position.y=(i+1)*10;//Filas
+        TEXTURA.malla2.matrixAutoUpdate=false;
+        TEXTURA.malla2.updateMatrix();
+        TEXTURA.malla2.receiveShadow=true;
+        g1.add(TEXTURA.malla2);
+        k++;
+    }
+   k++;
+   }
+TEXTURA.escena.add(TEXTURA.malla2);
 }
-  //Ceramica negra
+//Ceramica negra
 TEXTURA.retrollamadacnegra=function(textura){
   var material=new THREE.MeshBasicMaterial({map:textura});
   //Partes negras
   //Creación del grupo del tablero
-    var g1=new THREE.Group();
+    var g2=new THREE.Group();
     var k=0;
     for (var i=0;i<8;i++){
     for(var j=0;j<8;j++){
      if(k%2==0){
-     TEXTURA.malla=new THREE.Mesh(bloque,material);
+     TEXTURA.malla3=new THREE.Mesh(bloque,material);
     }
-    TEXTURA.malla.position.x=(j+1)*10;//Columnas
-    TEXTURA.malla.position.y=(i+1)*10;//Filas
-    TEXTURA.malla.matrixAutoUpdate=false;
-    TEXTURA.malla.updateMatrix();
-    TEXTURA.malla.receiveShadow=true;
-    g1.add(TEXTURA.malla);
+    TEXTURA.malla3.position.x=(j+1)*10;//Columnas
+    TEXTURA.malla3.position.y=(i+1)*10;//Filas
+    TEXTURA.malla3.matrixAutoUpdate=false;
+    TEXTURA.malla3.updateMatrix();
+    TEXTURA.malla3.receiveShadow=true;
+    g2.add(TEXTURA.malla3);
     k++;
   }
 k++;
 }
-  TEXTURA.escena.add(TEXTURA.malla);
+TEXTURA.escena.add(TEXTURA.malla3);
 }
-  //Madera
+//Madera
 TEXTURA.retrollamadamadera=function(textura){
   var material=new THREE.MeshBasicMaterial({map:textura});
-  TEXTURA.malla4=new THREE.Mesh();
-  TEXTURA.escena.add(TEXTURA.malla4);  
+  //Creación del grupo del borde (Alto)
+  var g3= new THREE.Group();
+  for(var l=0;l<10;l++){//columnas
+    for(var m=0;m<2;m++){//filas
+    var malla4= new THREE.Mesh(bloque,material);
+    if(m==1){
+     malla4.position.y=90;
+    }
+    malla4.position.x=(l*10);
+    malla4.matrixAutoUpdate = false;
+    malla4.updateMatrix();
+    malla4.receiveShadow=true;
+    g3.add(malla4);
+    }
+  }
+  //Creación del grupo del borde lateral (Ancho)
+  var g4=new THREE.Group();
+  for (var n=1;n<9;n++){//Filas
+    for (var o=0;o<2;o++){//Columnas
+    var malla5=new THREE.Mesh(bloque,material);
+    if (o==1){
+      malla5.position.x=90;
+    }
+    malla5.position.y=(n)*10
+    malla5.matrixAutoUpdate = false;
+    malla5.updateMatrix();
+    malla5.receiveShadow=true;
+    g4.add(malla5);
+    }
+  }
+TEXTURA.escena.add(TEXTURA.malla4);
+TEXTURA.escena.add(TEXTURA.malla5);
 }
 
 TEXTURA.setup=function(){
-  
-}
+  //Creación de la escena
+  TEXTURA.escena=new Three.Scene();
+  //Cargadores de las texturas
+  var cargador=new THREE.TextureLoader();
+  var cargador1=new THREE.TextureLoader();
+  var cargador2=new THREE.TextureLoader();
+  var cargador3=new THREE.TextureLoader();
+  var cargador4=new THREE.TextureLoader();
+  //Configuración de las imagenes
+  cargador.load("marmolnegro.jpg",TEXTURA.retrollamadamblanco);
+  cargador1.load("marmolblanco.jpg",TEXTURA.retrollamadamnegro);
+  cargador2.load("ceramicablanca.png",TEXTURA.retrollamadacblanca);
+  cargador3.load("ceramicanegra.jpg",TEXTURA.retrollamadacnegra);
+  cargador4.load("madera.jpg",TEXTURA.retrollamadamadera);
+  //Creación de la cámara
+  TEXTURA.camara=new THREE.PerspectiveCamera();
+  TEXTURA.camara.position.z=130;
+  TEXTURA.camara.position.x=45;
+  TEXTURA.camara.position.y=45;
+  TEXTURA.escena.rotateX(-Math.PI/6)
+  //Creación del lienzo y renderizador
+  var lienzo= document.getElementById("Tablero-ajedrez");
+  TEXTURA.renderizador=new THREE.WebGLRenderer({canvas:lienzo,antialias:true});
+  }
 
 cargador1.load(imagen1,retro1)
 cargador4.load(imagen4,retro4)
@@ -127,7 +215,9 @@ function setup(){
   Camara
 }
 
-function loop(){
-  Request...Frame(loop)
-  setuo();
+TEXTURA.loop=function(){
+  requestAnimationFrame(TEXTURA.loop);
+  TEXTURA.renderizador.render(TEXTURA.escena,TEXTURA.camara);
 }
+TEXTURA.setup();
+TEXTURA.loop();
