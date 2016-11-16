5a015494125df25830d0a3bd90435a57e8429966
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-11-15 18:32:29 -0600

    Update prueba_importacion.js

--- RV/prueba_importacion.js
+++ RV/prueba_importacion.js
@@ -3,16 +3,16 @@ var escena=new THREE.Scene();
 var loader = new THREE.JSONLoader(); 
 var createMesh = function( geometry )
 {
-    var zmesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
+    var zmesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial() );
     zmesh.position.set( 0, 0, 0 );
     zmesh.scale.set( 1, 1, 1 );
     zmesh.overdraw = true;
     escena.add(zmesh);
 };
-loader.load( "Cuboconcilindro.js", createMesh );
+loader.load("Cuboconcilindro.js", createMesh);
 
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=100;
+camara.position.z=20;
 
 var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
