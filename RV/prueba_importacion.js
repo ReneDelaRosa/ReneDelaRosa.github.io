a48dc238e05847452a37a40fb5b8a389d7a45602
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-11-18 07:44:58 -0600

    Update prueba_importacion.js

--- RV/prueba_importacion.js
+++ RV/prueba_importacion.js
@@ -1,26 +1,33 @@
 var escena=new THREE.Scene();
 var iluminacion= new THREE.PointLight(0xFFFFFF);
+var iluminacion2= new THREE.PointLight(0xFFFFFF);
+var iluminacion3= new THREE.PointLight(0xFFFFFF);
+var iluminacion4= new THREE.PointLight(0xFFFFFF);
 iluminacion.position.z=20;
+iluminacion2.position.x=20;
+iluminacion3.position.x=-20;
+iluminacion4.position.y=-20;
 
-// instantiate a loader
 var loader = new THREE.JSONLoader();
-// load a resource
-loader.load(
-	// resource URL
-	'./prueba1.js',
-	// Function when resource is loaded
-	function ( geometry, materials ) {
-		var material = new THREE.MultiMaterial( materials );
-		var object = new THREE.Mesh( geometry, material );
-		escena.add( object );
-	}
-);
+var createMesh = function( geometry )
+{
+    var zmesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial() );
+    zmesh.position.set( 0, 0, 0 );
+    zmesh.scale.set( 3, 3, 3 );
+    zmesh.overdraw = true;
+    escena.add( zmesh );
+};
+loader.load( "prueba1.js", createMesh );
+
 var material=new THREE.MeshBasicMaterial({color: 0x00ff00});
 var forma=new THREE.SphereGeometry(1);
 var malla=new THREE.Mesh(forma,material);
 forma.translate(0,5,0);
 escena.add(malla);
 escena.add(iluminacion);
+escena.add(iluminacion2);
+escena.add(iluminacion3);
+escena.add(iluminacion4);
 var camara=new THREE.PerspectiveCamera();
 
 camara.position.z=400;
