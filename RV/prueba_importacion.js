9c36ac64371bd0bd3ecb8a4039667b6d015343a9
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-11-18 07:40:59 -0600

    Update prueba_importacion.js

--- RV/prueba_importacion.js
+++ RV/prueba_importacion.js
@@ -25,7 +25,7 @@ var camara=new THREE.PerspectiveCamera();
 
 camara.position.z=15;
 
-var renderizador= new THREE.WebGLRenderer({alpha: true});
+var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
