f22b322cedff3f88efca6ce8f03d14407ca4a02d
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-11-28 15:20:04 -0600

    Create escenario.js

---
+++ RV/importacion/escenario.js
@@ -0,0 +1,22 @@
+var escena=new THREE.Scene();
+
+//var mtlLoader=new THREE.MTLLoader();
+	//mtlLoader.load("Pieza1.mtl",function(materials){
+		//materials.preload();
+		
+    var objLoader=new THREE.OBJLoader();
+		//objLoader.setMaterials(materials);
+		objLoader.load("Pieza1.obj",  function(mesh){
+			escena.add(mesh);
+			mesh.position.set(0,0,0);
+		//});
+		});
+
+var camara=new THREE.PerspectiveCamera();
+camara.position.z=15;
+camara.position.y=2.5;
+
+var renderizador=new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
