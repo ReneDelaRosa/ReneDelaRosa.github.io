bcd4d6ba40dd4edd46ca53d782d740b7449bfd00
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-12-13 13:11:38 -0600

    Update proyectoFinal.js

--- RV/proyecto/proyectoFinal.js
+++ RV/proyecto/proyectoFinal.js
@@ -414,14 +414,8 @@ BloqueAzul.prototype.act = function(environment){
 		      if(obstaculo.length >0)
 			{this.colision = 1;this.step=0;pIzqinit=0;pDerinit=0;}
  		      else
-		        {this.colision = 0;this.step=0.25;pIzqinit=0.05;pDerinit=-0.05;}
-		    }//fin prototype sense
-		
-		    TorreBlanca.prototype.act = function(environment){
-		      if (this.colision!=1){
-			if(torreblanca1.position.x<=bvtb1.position.x){
-			  torreblanca1.position.x += this.step;
-			  patitas1.Plcompleta.rotateZ(pIzqinit);
+		        {this.colision = 0;this.step=0.25;pIzqinit=0.05;pDerinit=-0.05;
+			 patitas1.Plcompleta.rotateZ(pIzqinit);
   			  if (patitas1.Plcompleta.rotation.z>0.785398){
                           pIzqinit=-pIzqinit;
                           }
@@ -436,11 +430,14 @@ BloqueAzul.prototype.act = function(environment){
                           pDerinit=-pDerinit;
                           }
 			}
-			else{
+		    }//fin prototype sense
+		
+		    TorreBlanca.prototype.act = function(environment){
+		      if (this.colision!=1){
+			if(torreblanca1.position.x<=bvtb1.position.x)
+			  torreblanca1.position.x += this.step;
+			else
 			  torreblanca1.position.x -= this.step;
-			  patitas1.Prcompleta.rotateZ(0);
-			  patitas1.Plcompleta.rotateZ(0);
-			}
 		      }//fin if posicion x
 		      if (this.colision!=1){
 			if(torreblanca1.position.z<=bvtb1.position.z){
