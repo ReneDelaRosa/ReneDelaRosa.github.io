bd505ea2daf5a7b6778816d239f38be211cc7666
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-12-13 13:43:10 -0600

    Update proyectoFinal.js

--- RV/proyecto/proyectoFinal.js
+++ RV/proyecto/proyectoFinal.js
@@ -411,9 +411,10 @@ BloqueAzul.prototype.act = function(environment){
 		    TorreBlanca.prototype.sense = function(environment){
 		      this.sensor.set(this.position,new THREE.Vector3(0,-1,0));
 		      var obstaculo = this.sensor.intersectObjects(bvtb1,true);
-		      if(obstaculo.length >0)
-			{this.colision = 1;this.step=0;pIzqinit=0;pDerinit=0}
- 		      else{this.colision = 0;this.step=0.25;pIzqinit=-0.05;pDerinit=0.05;
+		      if(obstaculo.length >0){
+			this.colision = 1;this.step=0;pIzqinit=0;pDerinit=0
+			}
+ 		      else{this.colision = 0;this.step=0.25;pIzqinit=0.05;pDerinit=-0.05;
 			  if (patitas1.Plcompleta.rotation.z>0.785398){
                           pIzqinit=-pIzqinit;
                           }
