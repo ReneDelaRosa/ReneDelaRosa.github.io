1dd0073e12855cc28b7f6e11090fc9f5221740d7
ReneDelaRosa <renedelarosapicazo@gmail.com>
2016-12-13 04:34:06 -0600

    Update proyectoFinal.js

--- RV/proyecto/proyectoFinal.js
+++ RV/proyecto/proyectoFinal.js
@@ -71,11 +71,11 @@ Piernar.merge(PiederMalla.geometry, PiederMalla.matrix);
 this.Prcompleta=new THREE.Mesh(Piernar,new THREE.MeshNormalMaterial());
 
 this.Plcompleta.position.x=x;
-this.Plcompleta.position.y=15;
-this.Plcompleta.position.z=-z;
-this.Prcompleta.position.y=-x;
-this.Prcompleta.position.y=15;
-this.Prcompleta.position.z=z;
+this.Plcompleta.position.y=10;
+this.Plcompleta.position.z=z-3.5;
+this.Prcompleta.position.y=x;
+this.Prcompleta.position.y=10;
+this.Prcompleta.position.z=z+3.5;
 	
 this.add(this.Plcompleta,this.Prcompleta);
 }
